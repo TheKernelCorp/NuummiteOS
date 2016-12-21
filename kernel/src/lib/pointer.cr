11549cc9e192d3eb2adb811246e9e3aab43f8c8e
Splitty <splittydev@gmail.com>
2016-12-21 10:51:14 +0100

    Fix [] operator on Pointer(T)

--- kernel/src/lib/pointer.cr
+++ kernel/src/lib/pointer.cr
@@ -3,7 +3,7 @@ struct Pointer(T)
     self + other.to_i64
   end
   def [](offset : Int) : T
-    self.value
+    (self + offset).value
   end
   def []=(offset : Int, value : T)
     (self + offset).value = value
