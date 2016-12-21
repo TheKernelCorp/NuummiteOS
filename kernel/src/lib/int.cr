e42aca9e9083ad552315e51a844bcab44c8b1a55
Splitty <splittydev@gmail.com>
2016-12-21 08:30:49 +0100

    Add Int struct

---
+++ kernel/src/lib/int.cr
@@ -0,0 +1,17 @@
+struct Int
+    def <<(count) unsafe_shl count end
+    def >>(count) unsafe_shr count end
+    def /(other : Int)
+        if other == 0
+            self
+        end
+        unsafe_div other
+    end
+    def times
+        i = 0
+        while i < self
+            yield i
+            i += 1
+        end
+    end
+end
