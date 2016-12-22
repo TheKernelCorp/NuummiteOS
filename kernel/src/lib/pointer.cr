8a20711f80f74ff3f081bafacce8266d724a01fa
Splitty <splittydev@gmail.com>
2016-12-22 02:19:12 +0100

    Add additional methods to Pointer

--- kernel/src/lib/pointer.cr
+++ kernel/src/lib/pointer.cr
@@ -2,10 +2,25 @@ struct Pointer(T)
   def +(other : Int)
     self + other.to_i64
   end
+  def -(other : Int)
+    self - other.to_i64
+  end
+  def ==(other : Int)
+    self == other.to_i64
+  end
+  def ==(other : Pointer(_))
+    self == other
+  end
   def [](offset : Int) : T
     (self + offset).value
   end
   def []=(offset : Int, value : T)
     (self + offset).value = value
+  end
+  def unwrap : T
+    self[0]
+  end
+  def null?
+    self == 0_u64
   end
 end
