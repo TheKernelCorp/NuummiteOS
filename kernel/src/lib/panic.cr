60f59c2d8795b3361ee66307db163f88d7550b76
Splitty <splittydev@gmail.com>
2016-12-26 03:19:06 +0100

    Improve kernel panic

--- kernel/src/lib/panic.cr
+++ kernel/src/lib/panic.cr
@@ -1,8 +1,24 @@
 def panic(file = __FILE__, line = __LINE__) : NoReturn
   puts "*** KERNEL PANIC"
+  panic_print_debug_information file, line
+  panic_halt_system
+end
+
+def panic(message : String, file = __FILE__, line = __LINE__) : NoReturn
+  print "*** KERNEL PANIC: "
+  puts message
+  panic_print_debug_information file, line
+  panic_halt_system
+end
+
+private def panic_print_debug_information(file, line)
   print "*** File: "
   puts file
-  asm("cli")
-  asm("hlt")
+  print "*** Line: "
+  puts line
+end
+
+private macro panic_halt_system
+  asm("cli; hlt")
   while true; end
 end
