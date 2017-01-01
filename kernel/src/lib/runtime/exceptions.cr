0904b85efa06e30c827e0164006433ea66cb8d91
Splitty <splittydev@gmail.com>
2017-01-01 21:10:21 +0100

    Add support for exception messages
    Signed-off-by: Splitty <splittydev@gmail.com>

--- kernel/src/lib/runtime/exceptions.cr
+++ kernel/src/lib/runtime/exceptions.cr
@@ -1,11 +1,11 @@
 class Exception
-  def self.new(message) : NoReturn
-    panic
+  def self.new(message, __file__ = __FILE__, __line__ = __LINE__) : NoReturn
+    panic message, __file__, __line__
   end
 end
 
 class TypeCastError < Exception
-  def self.new(message) : NoReturn
-    super message
+  def self.new(message, __file__ = __FILE__, __line__ = __LINE__) : NoReturn
+    super message, __file__, __line__
   end
 end
