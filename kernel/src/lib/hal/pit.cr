ac1254bdee50f83e26917c639790b2935da65d4d
Splitty <splittydev@gmail.com>
2017-01-09 00:08:34 +0100

    Change the PIT struct to a module
    Signed-off-by: Splitty <splittydev@gmail.com>

--- kernel/src/lib/hal/pit.cr
+++ kernel/src/lib/hal/pit.cr
@@ -4,12 +4,14 @@ private PIT_CH2_DATA = 0x42_u16
 private PIT_COMMAND = 0x43_u16
 private PIT_FREQ = 1193182_u32
 
-struct PIT
+module PIT
+  extend self
+
   @@ticks = 0_u64
   @@divisor = 0_u32
   @@frequency = 0_u32
 
-  def self.setup(frequency : Int)
+  def setup(frequency : Int)
     divisor = PIT_FREQ / frequency
     outb PIT_COMMAND, 0x36_u8
     outb PIT_CH0_DATA, divisor.to_u8
@@ -18,15 +20,15 @@ struct PIT
     @@frequency = frequency.to_u32
   end
 
-  def self.tick
+  def tick
     @@ticks += 1
   end
 
-  def self.ticks
+  def ticks
     @@ticks
   end
 
-  def self.sleep(ms : Int)
+  def sleep(ms : Int)
     end_tick_count = @@ticks + ((@@frequency * ms) / 1000)
     while @@ticks < end_tick_count
     end
