f77ccad0ddbd331f163a732496168a28850ce28e
Splitty <splittydev@gmail.com>
2017-01-10 01:40:11 +0100

    Fix reboot functionality (close #34)
    Signed-off-by: Splitty <splittydev@gmail.com>

--- kernel/src/lib/hal/pm.cr
+++ kernel/src/lib/hal/pm.cr
@@ -3,11 +3,11 @@ module PM
   extend self
 
   def reboot
-    IDT.disable_interrupts
+    asm("cli")
     loop do
       tmp = inb 0x64_u16
-      inb(0x60_u16) unless tmp & 1 == 0
-      break unless tmp & 2 == 0
+      inb(0x60_u16) unless (tmp & 1) == 0
+      break if (tmp & 2) == 0
     end
     outb 0x64_u16, 0xFE_u8
     asm("hlt; 1: hlt; jmp 1b")
