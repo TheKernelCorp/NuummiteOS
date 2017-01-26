37d13c07073b89eaa9bce67b9529e78bd473d17f
Splitty <splittydev@gmail.com>
2017-01-26 11:03:04 +0100

    Add basic heap structures
    Signed-off-by: Splitty <splittydev@gmail.com>

--- kernel/runtime/spec/heap_spec.cr
+++ kernel/runtime/spec/heap_spec.cr
@@ -1,5 +1,23 @@
 require "./spec_helper"
 
+def internal_bits(size : Int)
+  native_size = 32
+  {% if flag?(:x86_64) %}
+    native_size = 64
+  {% end %}
+  if size == native_size
+    yield
+  end
+end
+
+def bits32
+  internal_bits(32) { yield }
+end
+
+def bits64
+  internal_bits(64) { yield }
+end
+
 describe Heap do
   # TODO: Write tests
 
@@ -17,5 +35,12 @@ describe Heap do
     Heap.init maddr, limit
     Heap.memory_address.should eq maddr
     Heap.memory_limit.should eq maddr + limit
+  end
+
+  it "calculates the right limit" do
+    maddr = USize.new 0
+    Heap.init maddr
+    bits32 { Heap.memory_limit.should eq 0xFFFFFFFF_u32 }
+    bits64 { Heap.memory_limit.should eq 0xFFFFFFFFFFFFFFFF_u64 }
   end
 end
--- kernel/runtime/src/lib/heap.cr
+++ kernel/runtime/src/lib/heap.cr
@@ -1,9 +1,48 @@
+{% if flag?(:x86_64) %}
+  # A magic number used in the `BlockHeader`.
+  private MAGIC = USize.new(0x22A33ADD44C66CBB_u64)
+{% else %}
+  # A magic number used in the `BlockHeader`.
+  private MAGIC = USize.new(0x22A33ADD_u32)
+{% end %}
+
 private alias Block = LibHeap::Block
+private alias BlockHeader = LibHeap::BlockHeader
 
 private lib LibHeap
+
+  # Block header.
+  #
+  # Stores basic information about the block,
+  # including its size and connection to other blocks.
+  #
+  # NOTE: On 32-bit platforms the header size is 12 bytes.
+  # NOTE: On 64-bit platforms the header size is 24 bytes.
+  @[Packed]
+  struct BlockHeader
+    # A magic field used to verify the integrity of the block.
+    #
+    # NOTE: Always equal to `MAGIC`.
+    magic : USize
+
+    # The physical size of the block, including the header.
+    size : USize
+
+    # The next block in the list.
+    next_block : Block*
+  end
+
+  # Block.
+  #
+  # NOTE: On 32-bit platforms the block size is 16 bytes.
+  # NOTE: On 64-bit platforms the block size is 32 bytes.
   @[Packed]
   struct Block
-    phys_size : USize
+    # The block header.
+    header : BlockHeader
+
+    # The usable memory of the block.
+    entry : UInt8*
   end
 end
 
@@ -12,20 +51,25 @@ module Heap
   extend self
 
   # The start address.
-  @@maddr : USize = USize.new(0)
+  @@maddr : USize = USize.new 0
   # The absolute memory limit.
-  @@limit : USize = USize.new(0)
+  @@limit : USize = USize.new 0
   # The initialized flag.
   @@initialized : Bool = false
 
   # Initializes the heap.
+  #
   # The allocatable memory starts at _maddr_ and ends at _limit_.
   #
   # If no limit is supplied, the amount of allocatable memory
   # will only be limited by the platform integer width.
   def init(maddr : USize, limit : USize = 0)
     @@maddr = maddr
-    @@limit = limit == 0 ? USize.new(((1 << sizeof(USize)) - 1)) : maddr + limit
+    if limit == 0
+      @@limit = (USize.new(1) << (sizeof(USize) * 8)) - 1 
+    else
+      @@limit = maddr + limit
+    end
     @@initialized = true
   end
 
